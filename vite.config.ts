8c077ad126151402ccb2ae35e07a3f2911cb60fc
Gökay <gokayatalay2@outlook.com>
2024-03-16 16:33:50 +0100

    Testing new env variable approach

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 3000
-  }
-})
+
+export default ({ mode }: any) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    preview: {
+      port: 3000
+    }
+  })
+}
+
